027189393c158e6caff3b93d98a4eb82ccb45c24
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-01-07 16:11:21 -0500

    Use configparser for configuration and add support for multiple databases.

--- memrise_scraper.py
+++ memrise_scraper.py
@@ -1,66 +1,81 @@
+import configparser
 from lxml import html
 import requests
-
-# User-supplied configuration
-USERNAME = ''
-PASSWORD = ''
-DATABASE = ''
 
 # Constants
 # JH: not sure why the CSRF token is required, but can't get logins to work without it.
 CSRFTOKEN = 'a'
 LOGIN_URL = 'http://www.memrise.com/login/'
-FIELD_SEPARATOR = '|' 
+FIELD_SEPARATOR = '|'
+CONFIGURATION_FILE = 'memrise_scraper.ini'
 
 
-def fetch_content():
+def parse_configuration(configuration_file=CONFIGURATION_FILE):
+  config = configparser.ConfigParser()
+  config.read('memrise_scraper.ini')
+  return config['DEFAULT']['username'], config['DEFAULT']['password'], config['DEFAULT']['databases'].split(',')
+
+
+def fetch_content(username, password, databases):
   XPATH_PAGINATION = '//div[starts-with(@class, "pagination")]/ul/li/a/text()'
 
-  login_data = dict(username=USERNAME, password=PASSWORD, csrfmiddlewaretoken=CSRFTOKEN)
+  login_data = dict(username=username, password=password, csrfmiddlewaretoken=CSRFTOKEN)
   cookie_data = {'csrftoken': CSRFTOKEN}
 
   session = requests.session()
   session.post(LOGIN_URL, data=login_data, cookies=cookie_data)
 
-  page_number = 0
-  while (True):
-    page_number += 1
-    req = session.get(DATABASE + '?page=%d' % page_number)
-    this_page = req.text
+  for database_number, database in enumerate(databases, 1):
+    page_number = 0
+    while (True):
+      page_number += 1
+      # TODO(hammer): using logging module
+      print('Processing database %s, page %d' % (database, page_number))
+      req = session.get(database + '?page=%d' % page_number)
+      this_page = req.text
 
-    with open('page%s.html' % page_number, 'w') as ofile:
-      ofile.write(this_page)
+      with open('database%dpage%d.html' % (database_number, page_number), 'wt') as ofile:
+        ofile.write(this_page)
 
-    # Detect if we're at the end of a database
-    if 'Next' not in html.fromstring(this_page).xpath(XPATH_PAGINATION)[-1]:
-      break
+      # Detect if we're at the end of a database
+      if 'Next' not in html.fromstring(this_page).xpath(XPATH_PAGINATION)[-1]:
+        break
 
 
 def parse_content():
   XPATH_THINGS = '//tr[@class="thing"]'
   XPATH_FIELDS = 'td[starts-with(@class, "cell text")]/div/div'
 
-  page_number = 0
-  all_things = []
+  database_number = 0
   while (True):
-    page_number += 1
-    try:
-      with open('page%s.html' % page_number) as infile:
-        tree = html.fromstring(infile.read())
-        things = tree.xpath(XPATH_THINGS)
-        for thing in things:
-          fields = thing.xpath(XPATH_FIELDS)
-          all_things.append([field.text if field.text else '' for field in fields])
-    except:
+    database_number += 1
+    page_number = 0
+    all_things = []
+    while (True):
+      page_number += 1
+      print('Processing database %s, page %d' % (database_number, page_number))
+      try:
+        with open('database%dpage%d.html' % (database_number, page_number)) as infile:
+          tree = html.fromstring(infile.read())
+          things = tree.xpath(XPATH_THINGS)
+          for thing in things:
+            fields = thing.xpath(XPATH_FIELDS)
+            all_things.append([field.text if field.text else '' for field in fields])
+      except:
+        break
+
+    # Indicates that we've read the last database
+    if page_number == 1:
       break
 
-  # TODO(hammer): escape field separator
-  with open('all_things.csv', 'w') as ofile:
-    ofile.write('\n'.join([FIELD_SEPARATOR.join(thing) for thing in all_things]))
+    # TODO(hammer): escape field separator
+    with open('database%d.csv' % database_number, 'wt') as ofile:
+      ofile.write('\n'.join([FIELD_SEPARATOR.join(thing) for thing in all_things]))
 
 
 if __name__ == "__main__":
-  fetch_content()
+  username, password, databases = parse_configuration()
+  fetch_content(username, password, databases)
   parse_content()
 
 
